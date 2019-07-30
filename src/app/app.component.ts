8a82ff5d4f607b13fe0b9faa5b0d0d1f17d5400a
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-07-30 16:52:32 -0400

    Add header markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'trustvox-widgets-angular';
+  title = 'Trustvox Widgets';
 }
