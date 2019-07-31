90d4b6c3d8e02ecbeac9762a434fe57f265781d1
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-07-30 23:07:46 -0400

    Implement widget component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FromCategoryPipe } from './from-category.pipe';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { ProductReviewsComponent } from './product-reviews/product-reviews.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { FromCategoryPipe } from "./from-category.pipe";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { ProductReviewsComponent } from "./product-reviews/product-reviews.component";
 
 @NgModule({
   declarations: [
@@ -16,11 +16,8 @@ import { ProductReviewsComponent } from './product-reviews/product-reviews.compo
     ProductDetailsComponent,
     ProductReviewsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/product-reviews/product-reviews.component.ts
+++ src/app/product-reviews/product-reviews.component.ts
@@ -1,14 +1,80 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, Input, OnChanges, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
   selector: "app-product-reviews",
   templateUrl: "./product-reviews.component.html",
   styleUrls: ["./product-reviews.component.scss"]
 })
-export class ProductReviewsComponent implements OnInit {
-  @Input() product: object;
+export class ProductReviewsComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() product;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initializeTrustvox();
+  }
+
+  ngOnChanges() {
+    this.initializeTrustvox();
+    this.cleanupStyles(); // Only necessary for this demo. Please, don't put this in production
+  }
+
+  ngOnDestroy() {
+    this.cleanupTrustvox();
+    this.cleanupStyles(); // Only necessary for this demo. Please, don't put this in production
+  }
+
+  initializeTrustvox() {
+    this.setTrustvoxAttrs();
+    this.addTrustvoxScript();
+    this.cleanupTrustvox();
+    this.autoloadTrustvox();
+  }
+
+  setTrustvoxAttrs() {
+    window["_trustvox"] = [];
+
+    window["_trustvox"].push(
+      ["_storeId", this.product.storeId],
+      ["_productId", this.product.productId],
+      ["_customStyle", this.product.customStyle || {}]
+    );
+
+    if (this.product.name)
+      window["_trustvox"].push(["_productName", this.product.name]);
+
+    if (this.product.photos)
+      window["_trustvox"].push(["_productPhotos", this.product.photos]);
+  }
+
+  addTrustvoxScript() {
+    if (!this.isTrustvoxScriptLoaded()) {
+      const elem = window.document.createElement("script");
+      elem.src = "//static-staging.trustvox.com.br/sincero-staging/sincero.js";
+      elem.async = true;
+      document.head.appendChild(elem);
+    }
+  }
+
+  autoloadTrustvox() {
+    if (window["_trustvox_initializer"])
+      window["_trustvox_initializer"].initialize();
+  }
+
+  cleanupTrustvox() {
+    if (window["_trustvox_initializer"])
+      window["_trustvox_initializer"].cleanup();
+  }
+
+  isTrustvoxScriptLoaded() {
+    return !!document.querySelector(
+      "[src='//static-staging.trustvox.com.br/sincero-staging/sincero.js']"
+    );
+  }
+
+  // Only necessary for this demo. Please, don't put this in production
+  cleanupStyles() {
+    const customStyle = document.querySelector("style[data-jss]");
+    if (customStyle) document.head.removeChild(customStyle);
+  }
 }
